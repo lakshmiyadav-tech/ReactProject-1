cecd4e2586d4962a3095bb8f1ca11160fb929b79
Lakshmi Yadav <lakshmiyadav04082003@gmail.com>
2025-10-24 21:36:41 +0530

    hero component is introduced here

---
+++ components/hero.jsx
@@ -0,0 +1,16 @@
+const HeroSection = ()=>{
+    return (
+        <main className="hero">
+            <div className="hero-content"></div>
+            <div className="hero-image"></div>
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="hero-btn">
+                <button>Shop now</button>
+                <button>Category</button>
+            </div>
+        </main>
+    )
+}
+
+export  default HeroSection;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navigation from '../components/Nav'
+import HeroSection from '../components/hero'
 function App() {
 
   return (
     <>
     <Navigation/>
+    <HeroSection/>
     </>
   )
 }
